c2d2eafcda523c2446f500ae24da7c9e7df2f267
Case Prince <caseprince@gmail.com>
2014-06-19 00:03:16 -0400

    hackily coming together

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,13 +3,48 @@ define(
 		'jquery',
 		'isotope',
 		'packery',
-		'get-size'
+		'get-size',
+		'stellar',
+
+		//'packery-layout',
 	], 
 
 	function($, Isotope, Packery, getSize) {
 		console.log('jQuery %s', $().jquery);
 		console.log('isotope', Isotope);
 		console.log('Packery', Packery);
+
+	//	$("#content > div").each(function( i, item ){
+
+		var content = $('#content > div');
+		$.each(content, function (index, item) {
+
+		  var w = 1;
+		  if($(this).attr("data-w")){
+		  	window.console.log($(this).attr("data-w"));
+		  	w = $(this).attr("data-w");
+		  }
+		  var h = 1;
+		  if($(this).attr("data-h")){
+		  	window.console.log($(this).attr("data-h"));
+		  	h = $(this).attr("data-h");
+		  }
+
+		  //var img = $(this).find('a')[0].attr("href");
+		  var imageUrl =  $(this).find('a')[0].getAttribute('href');
+		  window.console.log(imageUrl);
+
+		  var title = $(this).find('h2')[0].innerHTML;
+
+
+		  var html = '<div class="element w-'+w+' h-'+h+'">';
+		      html += '<div style="background-image: url(\''+imageUrl+'\')">';
+		      html += '<h4>'+title+'</h4></div></div>';
+			
+			$("#container").append(html);
+
+
+		});
 
 
 		// overwrite Packery methods
@@ -55,52 +90,43 @@ define(
 		    gutter: 10
 		  });
 
-		/*jquery
-
-		var $container = $('#container').packery({
-		    itemSelector: '.element',
-		    columnWidth: 150,
-		    gutter: 20
-		  });*/
 
 		/*var container = $('#container')[0];
 		var iso = window.iso = new Isotope( container, {
-			layoutMode: 'fitRows',
-			masonry: {
-			      columnWidth: 50
-			    },
+			//layoutMode: 'fitRows',
+			//masonry: {
+			//      columnWidth: 50
+			//    },
 			layoutMode: 'packery',
+			packery: {
+			  gutter: 10
+			},
 			//itemSelector: '.mini-item',
 			//stamp: '.stamp'
 			getSortData: {
-			  // number: function( itemElem ) {
-			  //   return parseInt( getText( itemElem.querySelector('.number') ), 10 );
-			  // },
-			  // symbol: function( itemElem ) {
-			  //   return getText( itemElem.querySelector('.symbol') );
-			  // },
-			  // name: function( itemElem ) {
-			  //   return getText( itemElem.querySelector('.name') );
-			  // },
-			  // category: function( itemElem ) {
-			  //   return itemElem.getAttribute('data-category');
-			  // },
-			  
-
-			  number: '.number parseInt',
-			  symbol: '.symbol',
-			  name: '.name',
-			  category: '[data-category]',
-
-			  
+				  // number: function( itemElem ) {
+				  //   return parseInt( getText( itemElem.querySelector('.number') ), 10 );
+				  // },
+				  // symbol: function( itemElem ) {
+				  //   return getText( itemElem.querySelector('.symbol') );
+				  // },
+				  // name: function( itemElem ) {
+				  //   return getText( itemElem.querySelector('.name') );
+				  // },
+				  // category: function( itemElem ) {
+				  //   return itemElem.getAttribute('data-category');
+				  // },			  
+				  number: '.number parseInt',
+				  symbol: '.symbol',
+				  name: '.name',
+				  category: '[data-category]'			  
 			  }
-			}
-		});
+		});*/
 
 		//var options = document.querySelector('#options');
 
 
-		$('#options li a').click(function(event){
+		/*$('#options li a').click(function(event){
 			//});
 			//eventie.bind( options, 'click', function( event ) {
 			// use link's href, remove leading hash
@@ -116,11 +142,24 @@ define(
 		  return elem.textContent || elem.innerText;
 		}*/
 
-		$(".element").click(function(e){
+		$(".element").click(function(){
 			window.console.log($(this));
+			$("#overlay").fadeIn(100);
 		});
 
+		$("#overlay, #overlay .close").click(function(){
+			$("#overlay").fadeOut(100);
+		})
 
+		if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+			$.stellar({
+				horizontalScrolling: false
+			});
+		}
+
+
+		
+		
 
 
 	}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,16 @@ require.config({
 	baseUrl: 'scripts',
     paths: {
         jquery: '../bower_components/jquery/jquery',
+        stellar: '../bower_components/jquery.stellar/jquery.stellar',
         isotope: '../bower_components/isotope/dist/isotope.pkgd',
-        //packery: '../bower_components/isotope-packery/packery-mode.pkgd',
+        'packery-layout': '../bower_components/isotope-packery/packery-mode.pkgd',
+
         packery: '../bower_components/packery/dist/packery.pkgd',
         'get-size': '../bower_components/get-size/get-size',
         'get-style-property': '../bower_components/get-style-property',
 	},
 	shim: {
+        'stellar': ['jquery'],
 		'isotope': ['jquery'],
 		//'packery': ['isotope']
 	}
