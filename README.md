674de24d85c39ca73f3e80e5d59cf91193f21809
Case Prince <caseprince@gmail.com>
2025-09-24 19:32:51 -0400

    Refactor as Vite project

--- README.md
+++ README.md
@@ -2,36 +2,8 @@
 
 Hosted at [caseprince.com](http://caseprince.com)
 
-## Dev
+## Setup
 
-Install python 2 from https://www.python.org/downloads/release/python-2716/
+Install Node.js version 22.11.0 (or a compatible LTS release). In the project directory, run:
 
-Install node 16.16.0
-
-> npm install -g bower
-
-> bower install
-
-> npm install
-
-Install chocolatey: https://chocolatey.org/install
-..and an old version of ruby that still supports compass (https://stackoverflow.com/a/75923901)
- 
-> choco install ruby --version=3.1.3.1
-
-> gem install compass
-
-> npm install -g grunt-cli
-
-> grunt serve
-
-## Build
-
-> grunt build
-
-### Todo
-
-1. 86 bower (npm or yarn?)
-1. 86 jQuery (Vite? Vue?)
-2. Modernize CSS pipeline
-3. ~Host with github.io?~
+### `npm install` && `npm run dev`
---
+++ src/main.ts
@@ -0,0 +1,200 @@
+import $ from "jquery";
+import Isotope from "isotope-layout";
+import Packery from "packery";
+import getSize from "get-size";
+
+// These are ancient jQuery plugins that don't support ESM, so we have to do some manual registration
+import registerTyper from "./jquery/jquery.typer";
+import registerFancyBox from "./jquery/jquery.fancybox.js";
+
+registerTyper($);
+registerFancyBox(window, document, $);
+
+console.log("jQuery %s", $().jquery);
+console.log("isotope", Isotope);
+console.log("Packery", Packery);
+
+// Create the measurement node
+var scrollDiv = document.createElement("div");
+scrollDiv.className = "scrollbar-measure";
+document.body.appendChild(scrollDiv);
+
+// Get the scrollbar width
+var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+
+// Delete the DIV
+document.body.removeChild(scrollDiv);
+
+var content = $("#content > div");
+$.each(content, function (index, item) {
+    var w = $(this).attr("data-w") || 1,
+        h = $(this).attr("data-h") || 1,
+        title = $(this).find("h2")[0].innerHTML,
+        style = w == 1 ? "font-size:.7em;" : "";
+
+    var classes = "";
+    if ($(this).hasClass("fancybox")) {
+        classes += "fancybox ";
+    }
+    if ($(this).hasClass("iframe")) {
+        classes += "iframe ";
+    }
+
+    var imageUrl = $(this).find("a")[0].getAttribute("href"),
+        html =
+            '<div style="' +
+            style +
+            '" class="' +
+            classes +
+            "element w-" +
+            w +
+            " h-" +
+            h +
+            '" data-id="' +
+            $(this).attr("id");
+    if ($(this).data("href")) {
+        html += '" data-href="' + $(this).data("href");
+    }
+    html += '">';
+    html += "<div style=\"background-image: url('" + imageUrl + "')\">";
+    html += "<h4>" + title + "</h4></div></div>";
+
+    $("#container").append(html);
+});
+
+// Centers packery layout
+var __resetLayout = Packery.prototype._resetLayout;
+Packery.prototype._resetLayout = function () {
+    __resetLayout.call(this);
+    var parentSize = getSize(this.element.parentNode);
+    var colW = this.columnWidth + this.gutter;
+    this.fitWidth =
+        Math.floor((parentSize.innerWidth + this.gutter) / colW) * colW;
+    this.packer.width = this.fitWidth;
+    this.packer.height = Number.POSITIVE_INFINITY;
+    this.packer.reset();
+};
+
+Packery.prototype._getContainerSize = function () {
+    var emptyWidth = 0;
+    for (var i = 0, len = this.packer.spaces.length; i < len; i++) {
+        var space = this.packer.spaces[i];
+        if (space.y === 0 && space.height === Number.POSITIVE_INFINITY) {
+            emptyWidth += space.width;
+        }
+    }
+    return {
+        width: this.fitWidth - this.gutter,
+        height: this.maxY - this.gutter,
+    };
+};
+
+// always resize
+Packery.prototype.resize = function () {
+    this.layout();
+};
+
+var container = document.querySelector("#container");
+var pckry = new Packery(container, {
+    itemSelector: ".element",
+    columnWidth: 120,
+    gutter: 10,
+});
+
+$(".element").click(function () {
+    if (!$(this).hasClass("fancybox") && $(this).data("href")) {
+        window.location.assign($(this).data("href"));
+        return;
+    }
+    var id = $(this).data("id");
+    window.location.hash = "#" + id;
+});
+
+$("#scrim, #overlay, .close").click(function (e) {
+    console.log(e.target);
+    if (
+        e.target.id === "scrim" ||
+        e.target.id === "overlay" ||
+        e.target.classList.contains("close")
+    ) {
+        clearHash();
+    }
+});
+
+function clearHash() {
+    var scrollY = window.scrollY;
+    var scrollX = window.scrollX;
+
+    window.location.hash = "";
+
+    $("#overlay, #scrim").hide();
+    $("body").removeAttr("style");
+
+    window.scrollTo(scrollX, scrollY);
+}
+
+function updateHash() {
+    var id = window.location.hash;
+    if (id) {
+        id = id.slice(1);
+        var $content = $("#" + id),
+            $thumb = $('*[data-id="' + id + '"]');
+        if ($('*[data-id="' + id + '"]').hasClass("fancybox")) {
+            $.fancybox({
+                title: $content.data("caption") || "",
+                href: $content.data("href"),
+                type: $thumb.hasClass("iframe") ? "iframe" : "image",
+                afterClose: function () {
+                    clearHash();
+                },
+                openSpeed: 0,
+                closeSpeed: 0,
+                helpers: {
+                    overlay: {
+                        speedOut: 0,
+                    },
+                },
+            });
+        } else {
+            $("#content > div").hide(0);
+            $content.show(0);
+            $("#overlay, #scrim").fadeIn(50);
+            $("#overlay").scrollTop(0);
+            $("body").css({
+                overflow: "hidden",
+                "margin-right": 0,
+            });
+        }
+    } else {
+        if ($.fancybox && $.fancybox.close) {
+            $.fancybox.close(true);
+        }
+    }
+}
+window.onhashchange = updateHash;
+updateHash();
+
+if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    if ($.stellar) {
+        $.stellar({
+            horizontalScrolling: false,
+        });
+    }
+}
+
+$(".thumb, .fancythumb").fancybox({
+    openSpeed: 0,
+    closeSpeed: 0,
+    helpers: {
+        overlay: {
+            speedOut: 0,
+        },
+    },
+});
+$("a.mh").fancybox({ width: 760 });
+
+$.typer.options.typerInterval = 7500;
+$.typer.options.typeDelay = 30;
+$("[data-typer-targets]").typer();
+
+$("#brands, footer").delay(1000).show();
